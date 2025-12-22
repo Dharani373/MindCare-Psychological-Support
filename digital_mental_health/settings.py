c09a5657dff0e982ea037b18912e1b1d4e7b1334
Dharani373 <krdharani777@gmail.com>
2025-12-22 23:21:19 +0530

    added Secret_Key

--- digital_mental_health/settings.py
+++ digital_mental_health/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
